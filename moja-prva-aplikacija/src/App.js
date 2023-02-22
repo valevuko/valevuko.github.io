9642163324f760eadcf469fc811a3e774288d45b
valevuko <valentinavukovic0102@gmail.com>
2023-02-22 20:42:40 +0100

    adding

--- moja-prva-aplikacija/src/App.js
+++ moja-prva-aplikacija/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+// import Komponenta1 from './Components/Komponenta1';
+import { Komponenta1, Komponenta2, Komponenta3, Komponenta4 } from "./Components";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Komponenta1 />
+      <Komponenta2 />
+      <Komponenta3 />
+      <Komponenta4 />
+    </div>
+  );
 }
 
 export default App;
