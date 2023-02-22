48bcdc60f4ede87a70f89788d1f42904de1fa42e
valevuko <valentinavukovic0102@gmail.com>
2023-02-22 21:34:12 +0100

    5-jsx

--- moja-prva-aplikacija/src/App.js
+++ moja-prva-aplikacija/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const name = "Valentina";
+  const user = {
+    firstName: "Valentina",
+    lastName: "Horvat",
+  };
+
+  const element0 = <h1>Hello</h1>;
+  const element1 = <p>{name}</p>;
+  const element2 = <p>{user.lastName,}, {user.firstName}</p>;
+
+  return (
+    <div>
+      {element0}
+      {element1}
+      {element2}
+    </div>
+  );
 }
 
 export default App;
