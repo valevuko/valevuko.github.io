8f96fd9074b7488ab68190c1f3a814e31689aed2
valevuko <valentinavukovic0102@gmail.com>
2023-02-28 19:06:38 +0100

    adding

--- moja-prva-aplikacija/src/App.js
+++ moja-prva-aplikacija/src/App.js
@@ -1,7 +1,35 @@
-import "./App.css";
+import './App.css';
+import React from 'react';
+import { UserChildren, UserFunction, UserClass } from './Components';
 
-function App() {
-  return <div></div>;
+class App extends React.Component {
+
+  state = {
+    users: [ // sada "users" nije polje unutar App komponente, već je dio state-a
+      { name: "Ivana", age: 25 },
+      { name: "Goran", age: 22 },
+      { name: "Jasenka", age: 55 },
+    ]
+  }
+
+  render() {
+
+   const { users } = this.state; // JS object destructuring
+
+    return (
+      <div>
+        <UserFunction user={users[0]} />
+        <UserClass user={users[1]} />
+        <UserChildren>Moje ime je {users[2].name} i imam {users[2].age} godina.</UserChildren>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/* Početno stanje aplikacije je završno stanje aplikacije iz vježbe 5.8 i nalazi se u mapi "Pocetno stanje".
+
+	1. U vježbi je potrebno definirati polje korisnici iz prethodne vježbe unutar stanja (statea) komponente App
+	2. Prikazati na ekranu vrijednosti iz stanja (statea)
+ */
