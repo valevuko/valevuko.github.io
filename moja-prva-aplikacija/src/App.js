2e33206253c2d5172f42080869502f95360e7e55
valevuko <valentinavukovic0102@gmail.com>
2023-03-06 20:23:15 +0100

    adding forms

--- moja-prva-aplikacija/src/App.js
+++ moja-prva-aplikacija/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+/* import React from "react";  jedan način*/
+import { useState } from "react";
+
+//input>-controle
 
 function App() {
-  return <div></div>;
+  /*   const [nesto, setNesto] = React.useState();jedan način */
+
+  const [name, setName] = useState("");
+  //ako očekujemo stringu onda u zagradu trebamo staviti navodnike
+  function handleSubmit(event) {}
+
+  function handleNameChange(event) {
+    const value = event.target.value;
+    setName(value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Ime:
+        <input onChange={handleNameChange} value={name} type={"text"}></input>
+      </label>
+      <input type={"submit"} value={"Potvrdi"}></input>
+    </form>
+  );
 }
 
 export default App;
+
+/* Potrebno napraviti React aplikaciju koja će imati sljedeće:
+1. Potrebno je napraviti formu koja će imati traku za unos teksta i gumb za potvrdu unosa
+2. Korisničkom interakcijom potrebno je spremati tekst u stanje (state) komponente
+3. Pritiskom Pocetno stanjena tipku, potrebno je ispisati tekst na ekranu */
